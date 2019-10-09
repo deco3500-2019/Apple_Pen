9f459861f61cd8f0834f3e013ec05aac9e594887
Scott <scotthg@stud.ntnu.no>
2019-10-09 19:00:35 +1000

    added express server template

--- src/client/Client.js
+++ src/client/Client.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Route } from "react-router-dom";
 
 export default class Client extends Component {
-	render() {
+	render(){
 		return (<div> This is the students screen. </div>);
 	}
 }
