bc6428f775c11a477982c94f6a90b189cf27bf6b
Scott <scotthg@stud.ntnu.no>
2019-10-10 12:54:39 +1000

    added simple form to communicate with server

--- src/client/Client.js
+++ src/client/Client.js
@@ -1,8 +1,42 @@
 import React, { Component } from "react";
 import { Route } from "react-router-dom";
+import axios from "axios";
 
 export default class Client extends Component {
-	render(){
-		return (<div> This is the students screen. </div>);
+
+	state = {
+		text: "",
+		response: ""
 	}
+
+	render(){
+		return (
+		<div>
+			<form onSubmit={this.handleSubmit}>
+				<label htmlFor="new-todo">
+					Talk to teacher: 
+				</label>
+				<input
+					id="new-todo"
+					onChange={this.handleChange}
+					value={this.state.text}
+				/>
+				<button>
+					send text
+				</button>
+			</form>
+			<h3>{this.state.response}</h3>
+		</div>
+		)
+	}
+
+	handleChange = e => this.setState({ text: e.target.value })
+
+	handleSubmit = e => {
+		e.preventDefault();
+		axios.get("/server")
+			.then(res => {this.setState({ response: res.data });})
+			.catch(err => console.log(err));
+	}
+	
 }
