f3aa9d25861f5c94ed62b3bf75035a575e97cd62
Scott <scotthg@stud.ntnu.no>
2019-10-13 18:57:06 +1000

    #9 - added methods accessing data.json and returning object

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const cors = require("cors");
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path')
+
 const app = express()
 const port = 4000
 
@@ -24,3 +27,28 @@ app.post('/reply',
 
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+//---------------------DATA ACCESS METHODS BELOW-----------------------------------------------------------
+
+const getDataobject = () => {
+	const jsonString = fs.readFile(path.resolve(__dirname, 'data.json'), 'utf8', (err, jsonString) => {
+		if (err) {
+			console.log("File read failed:", err)
+			return
+		}
+		console.log('File data:', jsonString)
+		return jsonStringToObject(jsonString);
+	})
+}
+
+const jsonStringToObject = jsonString => {
+	try {
+		const object = JSON.parse(jsonString)
+		console.log("Resulting object from string:", object)
+		return object
+	} catch (err) {
+		console.log('Error parsing JSON string:', err)
+	}
+}
+
+getDataobject();
