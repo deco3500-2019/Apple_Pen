901682261516de0c7a47d8ac1ed332d466b89136
Scott <scotthg@stud.ntnu.no>
2019-10-22 09:45:11 +1000

    added a log

--- server/server.js
+++ server/server.js
@@ -17,6 +17,7 @@ app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, '../build')));
 
 app.get('/*', function (req, res) {
+	console.log("Browser requested index.html");
 	res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
