d3dacab79dd49df4d46da7e613cc5891d48a901d
Scott <scotthg@stud.ntnu.no>
2019-10-21 19:11:49 +1000

    fixed serve path

--- server/server.js
+++ server/server.js
@@ -14,9 +14,9 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
-app.get('/', function (req, res) {
+app.get('/*', function (req, res) {
 	res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
