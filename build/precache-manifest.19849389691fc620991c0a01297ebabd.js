2c405ea0530c8304fc670586816ca3509829c77a
Scott <scotthg@stud.ntnu.no>
2019-10-23 12:58:19 +1000

    added build to repo

---
+++ build/precache-manifest.19849389691fc620991c0a01297ebabd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2ef69cbb7aa42d86ca6bc6261128d96",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a75860821a165171ce44",
+    "url": "/static/js/2.9b3973f5.chunk.js"
+  },
+  {
+    "revision": "fe84de6ff476d0d36ce8",
+    "url": "/static/js/main.d70a0c07.chunk.js"
+  },
+  {
+    "revision": "8f4ff50044af68e119b9",
+    "url": "/static/js/runtime-main.07c76ea9.js"
+  }
+]);
